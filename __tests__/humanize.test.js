a32fd189a83913602c7bfb390ad5cf088f7ce691
Slaventy <slavik.shvets@do-it.co>
2017-02-12 16:04:46 +0200

    implemented humanize() from ruby

---
+++ __tests__/humanize.test.js
@@ -0,0 +1,10 @@
+import humanize from '../src/humanize';
+
+test('must replace underscores with spaces and capitalize first word', () => {
+  const sampleText = 'hello_world';
+  const expectedText = 'Hello world';
+
+  const humanizedText = humanize(sampleText);
+
+  return expect(humanizedText).toBe(expectedText); 
+});
---
+++ src/humanize.js
@@ -0,0 +1,8 @@
+const humanize = (text) => {
+  const wordArr = text.split('_');
+  wordArr[0] = wordArr[0].charAt(0).toUpperCase() + wordArr[0].slice(1);
+
+  return wordArr.join(' ');
+}
+
+export default humanize;
